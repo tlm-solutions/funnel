2b4480fde563596971c0d250ba2ad6502d19ec2e
revol-xut <revol-xut@protonmail.com>
2022-07-10 21:47:24 +0200

    new funnel implementation

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -2,21 +2,16 @@
 //
 use telegrams::{R09WebSocketTelegram, R09GrpcTelegram};
 
-use stop_names::TransmissionPosition;
-
-use futures_util::stream::{SplitSink, SplitStream};
 use tokio::net::TcpStream;
 use {
-    async_tungstenite::{accept_async, tokio::TokioAdapter},
-    asynchronous_codec::{Framed, LinesCodec},
-    futures::{executor::block_on, SinkExt, StreamExt},
     serde::{Deserialize, Serialize},
     std::{
         env,
         sync::{Arc, Mutex},
     },
     tokio::net::TcpListener,
-    ws_stream_tungstenite::*,
+    bus::{Bus, BusReader},
+    tokio::io::AsyncWriteExt
 };
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -37,20 +32,7 @@ impl Filter {
     }
 }
 
-pub struct UserState {
-    filter: Option<Filter>,
-    dead: bool,
-}
-
-pub type ProtectedState = Arc<Mutex<UserState>>;
-
-pub struct Socket {
-    write_socket: SplitSink<Framed<WsStream<TokioAdapter<TcpStream>>, LinesCodec>, String>,
-    read_socket: SplitStream<Framed<WsStream<TokioAdapter<TcpStream>>, LinesCodec>>,
-    state: ProtectedState,
-}
-
-pub async fn connection_loop(mut connections: ConnectionPool) {
+pub async fn connection_loop(bus: Arc<Mutex<Bus<R09GrpcTelegram>>>) {
     let default_websock_port = String::from("127.0.0.1:9001");
     let websocket_port = env::var("DEFAULT_WEBSOCKET_HOST").unwrap_or(default_websock_port);
 
@@ -58,140 +40,25 @@ pub async fn connection_loop(mut connections: ConnectionPool) {
 
     while let Ok((tcp, addr)) = server.accept().await {
         println!("New Socket Connection {}!", addr);
-
-        let s = accept_async(TokioAdapter::new(tcp))
-            .await
-            .expect("ws handshake");
-        let ws = WsStream::new(s);
-
-        // spliting the socket into read and write component
-        let (writer, reader) = Framed::new(ws, LinesCodec {}).split();
-
-        let state: ProtectedState = Arc::new(Mutex::new(UserState {
-            dead: false,
-            filter: None,
-        }));
-
-        connections
-            .push(Socket {
-                write_socket: writer,
-                read_socket: reader,
-                state: state.clone(),
-            })
-            .await;
+        let new_receiver = bus.lock().unwrap().add_rx();
+        tokio::spawn(accept_connection(tcp, new_receiver));
     }
 }
 
-impl Socket {
-    pub async fn read(&mut self) -> bool {
-        match self.read_socket.next().await.transpose() {
+
+async fn accept_connection(mut stream: TcpStream, mut receiver: BusReader<R09GrpcTelegram>) {
+    loop {
+        let data = receiver.recv().unwrap();
+        let json_to_string = serde_json::to_string(&data).unwrap();
+
+        match stream.write(json_to_string.as_bytes()).await {
             Err(e) => {
-                println!("Reading socket error {:?}", e);
-                self.state.lock().unwrap().dead = true;
-                true
+                println!("Error {:?}", e);
+                break;
             }
-            Ok(data) => {
-                let unwrapped_data = data.unwrap();
-                println!("data: {:?}", &unwrapped_data);
-                match serde_json::from_str(&unwrapped_data) {
-                    Ok(parsed_struct) => {
-                        println!("Updating Filter!");
-                        self.state.lock().unwrap().filter = Some(parsed_struct);
-                    }
-                    _ => {}
-                }
-                false
-            }
-        }
-    }
-    pub async fn write(&mut self, telegram: &R09GrpcTelegram, stop: &TransmissionPosition) -> bool {
-        {
-            let state = self.state.lock().unwrap();
-            if state.filter.is_some() && !state.filter.as_ref().unwrap().fits(telegram) {
-                return false;
-            }
-        }
+            Ok(_) => {}
 
-        let sock_tele = R09WebSocketTelegram {
-            reduced: telegram.clone(),
-            meta_data: stop.clone(),
-        };
-
-        let serialized = serde_json::to_string(&sock_tele).unwrap();
-
-        match self.write_socket.send(serialized).await {
-            Ok(_) => false,
-            Err(err) => {
-                println!("Error while writing into socket {:?}", err);
-                self.state.lock().unwrap().dead = true;
-                true
-            }
-        }
-    }
-}
-
-impl UserState {
-    pub fn new() -> UserState {
-        UserState {
-            filter: None,
-            dead: false,
-        }
-    }
-}
-
-#[derive(Clone)]
-pub struct ConnectionPool {
-    connections: Arc<Mutex<Vec<Socket>>>,
-}
-
-impl ConnectionPool {
-    pub fn new() -> ConnectionPool {
-        ConnectionPool {
-            connections: Arc::new(Mutex::new(Vec::new())),
         }
     }
 
-    pub fn clone(&mut self) -> ConnectionPool {
-        ConnectionPool {
-            connections: Arc::clone(&self.connections),
-        }
-    }
-
-    pub async fn write_all(&self, extracted: &R09GrpcTelegram, stop_meta_information: &TransmissionPosition) {
-        let mut dead_sockets = Vec::new();
-        let mut unlocked_self = self.connections.lock().unwrap();
-        
-        println!("Telegram: {:?}", extracted);
-        for (i, socket) in unlocked_self.iter_mut().enumerate() {
-            println!("Writing to socket {}", i);
-            match block_on(tokio::time::timeout(
-                    tokio::time::Duration::from_secs(1),
-                    socket.write(&extracted, &stop_meta_information))) {
-                Ok(err) => {
-                    if err {
-                        dead_sockets.push(i);
-                        continue;
-                    }
-                }
-                Err(_) => {
-                    println!("timeout write {}", i);
-                    dead_sockets.push(i);
-                }
-            }
-        }
-
-        // removing dead sockets
-        let mut remove_count = 0;
-        for index in dead_sockets.iter() {
-            println!("Removing {}", index);
-            unlocked_self.remove(index - remove_count);
-            remove_count += 1;
-        }
-    }
-
-    pub async fn push(&mut self, sock: Socket) {
-        let mut unlocked_self = self.connections.lock().unwrap();
-        unlocked_self.push(sock);
-        println!("ConnectionPool size: {}", unlocked_self.len());
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,47 +10,38 @@ pub use telegrams::{
     ReceivesTelegrams
 };
 
-pub use connection::{connection_loop, ConnectionPool, ProtectedState, Socket };
-pub use stop_names::{InterRegional, TransmissionPosition, TelegramType};
+pub use stop_names::{
+    InterRegional, 
+    TransmissionPosition, 
+    TelegramType
+};
+
+use connection::connection_loop;
 
 use std::env;
 use tonic::{transport::Server, Request, Response, Status};
+use bus::Bus;
+use std::sync::{Mutex, Arc};
 
 
 #[derive(Clone)]
 pub struct TelegramProcessor {
-    pub connections: ConnectionPool,
-    pub stops_lookup: InterRegional
+    pub stops_lookup: InterRegional,
+    sender: Arc<Mutex<Bus<R09GrpcTelegram>>>
 }
 
 impl TelegramProcessor {
-    fn new(list: ConnectionPool) -> TelegramProcessor {
+    fn new(bus: Arc<Mutex<Bus<R09GrpcTelegram>>>) -> TelegramProcessor {
         let default_stops = String::from("../stops.json");
         let stops_file = env::var("STOPS_FILE").unwrap_or(default_stops);
 
         println!("Reading File: {}", &stops_file);
-        let res = InterRegional::from(stops_file.as_str()).expect("cannot parse stops.json");
+        let res = InterRegional::from(stops_file.as_str())
+            .expect("cannot parse stops.json");
 
         TelegramProcessor {
-            connections: list,
             stops_lookup: res,
-        }
-    }
-
-    fn stop_meta_data(&self, junction: u32, region: u32) -> TransmissionPosition {
-        match self.stops_lookup.get_approximate_position(&region, &junction) {
-            Some(found_stop) => {
-                return found_stop.clone();
-            }
-            _ => {}
-        }
-        TransmissionPosition {
-            dhid: None,
-            name: None,
-            telegram_type: TelegramType::DoorClosed,
-            direction: 0,
-            lat: 0f64,
-            lon: 0f64,
+            sender: bus
         }
     }
 }
@@ -60,12 +51,8 @@ impl ReceivesTelegrams for TelegramProcessor {
     async fn receive_r09(&self, request: Request<R09GrpcTelegram>,
     ) -> Result<Response<ReturnCode>, Status> {
         let extracted = request.into_inner().clone();
-        let stop_meta_information =
-            self.stop_meta_data(extracted.junction, extracted.region as u32);
 
-        self.connections
-            .write_all(&extracted, &stop_meta_information)
-            .await;
+        self.sender.lock().unwrap().broadcast(extracted);
 
         let reply = ReturnCode { status: 0 };
         Ok(Response::new(reply))
@@ -76,16 +63,15 @@ impl ReceivesTelegrams for TelegramProcessor {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let default_grpc_port = String::from("127.0.0.1:50051");
     let grpc_port = env::var("GRPC_HOST").unwrap_or(default_grpc_port);
-
     let addr = grpc_port.parse()?;
-    let list: ConnectionPool = ConnectionPool::new();
-    let list_clone = list.clone();
-
+    let global_bus = Arc::new(Mutex::new(Bus::new(20)));
+    
+    let arc_clone = Arc::clone(&global_bus);
     tokio::spawn(async move {
-        connection_loop(list_clone).await;
+        connection_loop(arc_clone).await;
     });
 
-    let telegram_processor = TelegramProcessor::new(list.clone());
+    let telegram_processor = TelegramProcessor::new(global_bus);
     Server::builder()
         .add_service(ReceivesTelegramsServer::new(telegram_processor))
         .serve(addr)
