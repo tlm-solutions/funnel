65fc36d6d472a0b49cc767fbbf95c90c7bb7cdc5
revol-xut <revol-xut@protonmail.com>
2022-08-13 15:26:49 +0200

    test run with classic tungstenite

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -2,17 +2,23 @@
 //
 use telegrams::{R09WebSocketTelegram, R09GrpcTelegram};
 
-use tokio::net::TcpStream;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use std::io::Write;
+//use tokio::net::TcpStream;
 use {
     serde::{Deserialize, Serialize},
     std::{
         env,
         sync::{Arc, Mutex},
     },
-    tokio::net::TcpListener,
+    //tokio::net::TcpListener,
     tokio::sync::broadcast,
-    tokio::io::AsyncWriteExt
+    //tokio::io::AsyncWriteExt,
+    tungstenite::accept
 };
+use tungstenite::WebSocket;
+use tungstenite::Message;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Filter {
@@ -36,10 +42,11 @@ pub async fn connection_loop(wrapped_tx: Arc<Mutex<broadcast::Sender<R09GrpcTele
     let default_websock_port = String::from("127.0.0.1:9001");
     let websocket_port = env::var("DEFAULT_WEBSOCKET_HOST").unwrap_or(default_websock_port);
 
-    let server = TcpListener::bind(websocket_port).await.unwrap();
+    let server = TcpListener::bind(websocket_port).unwrap();
 
-    while let Ok((tcp, addr)) = server.accept().await {
-        println!("New socket connection on address {}!", addr);
+    for stream in server.incoming() {
+        let mut websocket = accept(stream.unwrap()).unwrap();
+        //println!("New socket connection on address {}!", addr);
         let mut new_receiver: broadcast::Receiver<R09GrpcTelegram>;
         let receiver_count: usize;
 
@@ -50,17 +57,23 @@ pub async fn connection_loop(wrapped_tx: Arc<Mutex<broadcast::Sender<R09GrpcTele
         }
 
         println!("Current receiver count is {}.", receiver_count);
-        tokio::spawn(accept_connection(tcp, new_receiver));
+        tokio::spawn(accept_connection(websocket, new_receiver));
     }
 }
 
 
-async fn accept_connection(mut stream: TcpStream, mut receiver: broadcast::Receiver<R09GrpcTelegram>) {
+async fn accept_connection(mut stream: WebSocket<TcpStream>, mut receiver: broadcast::Receiver<R09GrpcTelegram>) {
     loop {
         let data = receiver.recv().await.unwrap();
         let json_to_string = serde_json::to_string(&data).unwrap();
+        //let msg = websocket.read_message().unwrap();
 
-        match stream.write(json_to_string.as_bytes()).await {
+        // We do not want to send back ping/pong messages.
+        //if msg.is_binary() || msg.is_text() {
+        //    websocket.write_message(msg).unwrap();
+        //}
+        
+        match stream.write_message(Message::Text(json_to_string)) {
             Err(e) => {
                 println!("Error {:?}", e);
                 break;
